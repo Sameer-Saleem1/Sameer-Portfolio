a9aad74984664ba1f1b591ca3dbddee3f600153c
Sameer Saleem <itsmetrax2.0@gmail.com>
2023-11-15 20:39:10 +0500

    Done with contact section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,11 @@ export default function Header() {
       <Navbar.Brand> <Link className='Navbar-Name' to={"/"}>Sameer</Link></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className='shadow-none border-0'/>
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="navbar-nav mx-auto">
+        <Nav className="navbar-nav ms-auto">
           <Nav.Link><Link className='nav-link Navbar-sections' to={"/"}><h2>Home</h2></Link></Nav.Link>
           <Nav.Link><Link className='nav-link Navbar-sections' to={"/about"}><h2>About</h2></Link></Nav.Link>
           <Nav.Link><Link className='nav-link Navbar-sections' to={"/project"}><h2>Projects</h2></Link></Nav.Link>
-          <Nav.Link><Link className='nav-link Navbar-sections' to={"/review"}><h2>Reviews</h2></Link></Nav.Link>
+          {/* <Nav.Link><Link className='nav-link Navbar-sections' to={"/review"}><h2>Reviews</h2></Link></Nav.Link> */}
           <Nav.Link><Link className='nav-link Navbar-sections' to={"/contact"}><h2>Contact</h2></Link></Nav.Link>
         </Nav>
       </Navbar.Collapse>
